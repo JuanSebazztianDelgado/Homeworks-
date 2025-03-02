f95017ae087706029400179168c4b35b4b3e410f
JuanSebazztianDelgado <juan_s.delgado_b@uao.edu.co>
2025-03-02 18:36:05 -0500

    Mi primer Commit

---
+++ Practicas ED2/my-vite-app/src/FirstApp.jsx
@@ -0,0 +1,12 @@
+import './FirstApp.css'
+
+const FirstApp = () => {
+    return(
+        <>
+        <h1> INTERPOOL </h1>
+        <span> 10 </span>
+        </>
+    )
+}
+
+export default FirstApp
