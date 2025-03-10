c380bb683cb20d823353b6e535df7963ab93ba2e
JuanSebazztianDelgado <juan_s.delgado_b@uao.edu.co>
2025-03-10 12:18:43 -0500

    Funcion Flecha

---
+++ Vite/vite-project/src/Arrow type.js
@@ -0,0 +1,7 @@
+const verificarParidad = (numero) => {
+    if (numero % 2 === 0) {
+        console.log(`${numero} es un número par.`);
+    } else {
+        console.log(`${numero} es un número impar.`);
+    }
+};
