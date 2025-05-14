f9b0c685a8c6612364d18cca13846615444e1a0e
JuanSebazztianDelgado <juan_s.delgado_b@uao.edu.co>
2025-05-13 20:45:30 -0500

    Song request SCSS

---
+++ Vite/vite-project/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Lista de reproducción</h1>
+    </header>
+  );
+};
+
+export default Header;
